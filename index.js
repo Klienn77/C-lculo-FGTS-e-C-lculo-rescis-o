10575d39035f8b7670f072290bd74cc699790b28
Fabiano <fabianompedroso@hotmail.com>
2025-01-22 12:35:48 -0300

    atualizado para os arquivos html e css

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 // Importa o módulo express
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Define uma rota de teste
+// Configura a pasta 'src' para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Define a rota principal para servir o arquivo index.html
 app.get('/', (req, res) => {
-  res.send('Aplicação rodando!');
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
 // Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-    
